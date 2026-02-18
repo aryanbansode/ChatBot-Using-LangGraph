ed321e7eb036499e2ea718b12be71820379b166b
Aryan Bansode <154400420+aryanbansode@users.noreply.github.com>
2026-02-18 10:18:14 +0530

    Add files via upload

---
+++ langgraph_backend.py
@@ -0,0 +1,67 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_groq import ChatGroq
+from langchain_core.messages import BaseMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+import sqlite3
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+# *************************************************************************************************
+
+# llm = ChatGroq(api_key=os.getenv(GROQ_API_KEY),
+#                model_name='openai/gpt-oss-120b')  # type:ignore
+llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), model_name='openai/gpt-oss-20b')
+# meta-llama/llama-4-scout-17b-16e-instruct
+
+# ************************************************************************************************
+
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# ***********************************************************************************************
+
+
+def chat_node(State: ChatState):
+
+    # take user query
+    messages = State['messages']
+
+    # send to llm
+    response = llm.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+# *********************************************************************************************
+
+
+conn = sqlite3.connect(database='chatboat.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+
+# ***********************************************************************************************
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START,  'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+# **********************************************************************************
+
+
+def retrive_thread():
+    all_threads = set()
+    for checkpoints in checkpointer.list(None):
+        all_threads.add(checkpoints.config['configurable']['thread_id'])
+    return list(all_threads)
+
+
+
+
